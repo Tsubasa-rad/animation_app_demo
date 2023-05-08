3bd84768afff66ca6d15684bbb2410afd722df8e
Tsubasa-rad <tyamamoto472@gmail.com>
2023-05-08 11:18:40 +0900

    init

--- lib/screens/listview_scroling/list_scroll_view.dart
+++ lib/screens/listview_scroling/list_scroll_view.dart
@@ -8,6 +8,20 @@ class ListScrollView extends StatefulWidget {
 }
 
 class _ListScrollViewState extends State<ListScrollView> {
+  late FixedExtentScrollController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FixedExtentScrollController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,18 +29,48 @@ class _ListScrollViewState extends State<ListScrollView> {
         title: const Text("list wheel Scroll View"),
         centerTitle: true,
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
+      body: // ListView
+          ListWheelScrollView.useDelegate(
+        // padding: const EdgeInsets.all(16),
         itemExtent: 250,
-        children: [
-          Card(
-            child: Center(
-              child: Text("1"),
-            ),
-            color: Colors.blue,
-          )
-        ],
+        physics: FixedExtentScrollPhysics(),
+        // onSelectedItemChanged: (index) =>
+        //     showToast('Selected Item: ${index + 1}'),
+        // squeeze: 0.7, //default 1.0
+        // offAxisFraction: -1.5,
+
+        // useMagnifier: true,
+        // magnification: 1.5,
+
+        //value between 0 --> 0.01
+        // perspective: 0.005,
+        // diameterRatio: 0.8, //default 2.0
+        childDelegate: ListWheelChildBuilderDelegate(
+            childCount: 10, builder: (context, index) => buildCard(index)),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_forward),
+        onPressed: () {
+          final nextIndex = controller.selectedItem + 1;
+          controller.animateToItem(
+            nextIndex,
+            duration: Duration(seconds: 1),
+            curve: Curves.easeInOut,
+          );
+        },
+      ),
+    );
+  }
+
+  Card buildCard(int item) {
+    return Card(
+      child: Center(
+        child: Text(
+          "${item}",
+          selectionColor: Colors.white,
+        ),
+      ),
+      color: Colors.blue,
     );
   }
 }
